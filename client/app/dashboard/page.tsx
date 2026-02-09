23ba72e3db2d0f64d75140dd45851ffd70bbcf1e
Dustin Johnson <d.l.johnson3009@gmail.com>
2026-02-09 00:20:54 -0500

    feat: re-establish initial events route structure for front end - remove dashboard routes for mvp simplicity - add create and edit routes to events, use conditionals for admin checks

--- client/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EventDashboard = () => {
-	return <div>EventDashboard</div>;
-};
-
-export default EventDashboard;
--- client/app/events/page.tsx
+++ client/app/events/page.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import NavBar from "../components/NavBar";
 
-const EventsPage = () => {
-	return <div>EventsPage</div>;
+interface Event {
+	id: number;
+	name: string;
+	description: string;
+	location: string;
+	date: string;
+	time: string;
+	organizationName: string;
+	signupCount: number; // How many signed up
+	userSignedUp: boolean; // Is current user signed up
+}
+
+interface User {
+	id: number;
+	name: string;
+	role: "admin" | "volunteer"; // Just two options!
+}
+
+const EventsPage = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const users: User[] = await res.json();
+
+	console.log(users);
+  
+	return (
+		<div>
+			<NavBar />
+			EventsPage
+		</div>
+	);
 };
 
 export default EventsPage;
