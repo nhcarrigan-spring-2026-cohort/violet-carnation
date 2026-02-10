d5c53b13869a546a4d7d8370cdbbcff2622c27b7
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-02-10 12:39:16 -0800

    fix: db schema syntax issue

--- api/utils/db_schema.py
+++ api/utils/db_schema.py
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS event_registrations (
     organization_id INTEGER NOT NULL,
     registration_time TEXT NOT NULL,
     PRIMARY KEY (user_id, organization_id, event_id)
+);
 CREATE TABLE IF NOT EXISTS events (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL, 
