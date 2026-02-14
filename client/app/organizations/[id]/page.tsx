6f22ae8a3d62744ba857b8a90a86fade6c06bb6e
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-02-13 21:36:15 -0800

    feat: add organization pages and sub routes
    - this makes me inclined to believe the event pages should be added under the organization routes

---
+++ client/app/organizations/[id]/page.tsx
@@ -0,0 +1,11 @@
+/**
+ * This page is for viewing a specific organization.
+ *
+ * Should show:
+ * - organization details
+ * - members of the organization
+ * - ability to edit organization details (if the user has permission)
+ */
+export default async function OrganizationPage() {
+  return <div>OrganizationPage</div>;
+}
---
+++ client/app/organizations/create/page.tsx
@@ -0,0 +1,13 @@
+/**
+ * This page is for creating a new organization.
+ *
+ * Any user can do this, upon creation they will be the owner of the organization and can invite others to join, as
+ * they are automatically made an admin
+ *
+ * Should show:
+ * - a form to enter organization details
+ * - ability to submit the form to create the organization
+ */
+export default function CreateOrganizationPage() {
+  return <div>CreateOrganizationPage</div>;
+}
