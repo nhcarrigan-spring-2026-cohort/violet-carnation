d7556c7fe2cd3bb53a87386065dcc0136916ae83
Adnan Akbar <adnanakbar.0207@gmail.com>
2026-02-19 13:55:49 +0000

    task: add category attribute to organization files

--- api/models/organization.py
+++ api/models/organization.py
@@ -7,16 +7,19 @@ class Organization(BaseModel):
     organization_id: PositiveInt
     name: str
     description: Optional[str] = None
+    category: str
     created_by_user_id: PositiveInt
 
 
 class OrganizationCreate(BaseModel):
     name: str
     description: Optional[str] = None
+    category: str
     user_id: PositiveInt
 
 
 class OrganizationUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
+    category: str
     user_id: PositiveInt
