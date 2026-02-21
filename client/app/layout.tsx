7f4016135f84cdc1532ab6124002b34123bd5826
Tiffany <35204671+TiffanyClark@users.noreply.github.com>
2026-02-21 07:58:32 -0600

    Apply suggestions from code review
    Co-authored-by: Brad <bradtaniguchi@gmail.com>

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "next-themes"
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
